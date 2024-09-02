ecb2262e8ab66caf64d8f6e6bdac597e0e33cd03
XING, Zhenghao <36613867+HarryHsing@users.noreply.github.com>
2024-09-02 23:55:12 +0800

    Update dataset_sr.py

--- dataset_sr.py
+++ dataset_sr.py
@@ -38,30 +38,44 @@ class TrainValDataset(Dataset):
         label_lab = cv2.cvtColor(label, cv2.COLOR_BGR2LAB)
 
         # Resize images
-        image_lab = cv2.resize(image_lab, (256, 256))
-        label_lab = cv2.resize(label_lab, (256, 256))
+        image_lab = cv2.resize(image_lab, (512, 512))
+        label_lab = cv2.resize(label_lab, (512, 512))
 
         # Convert to PIL Image for transformations
         image_lab = Image.fromarray(image_lab)
         label_lab = Image.fromarray(label_lab)
 
+        # max and min value of image_lab
+        # print("image_lab max: ", np.max(image_lab))
+        # print("image_lab min: ", np.min(image_lab))
+
         image_lab, label_lab = self.hflip(image_lab, label_lab)
 
-        label_lab = np.array(label_lab, dtype='float32') / 255.0
+        # label_lab = np.array(label_lab, dtype='float32') / 255.0
+        label_lab = np.array(label_lab, dtype='float32')
 
         image_nom = self.trans(image_lab)
+        # print("image_nom max: ", image_nom.max())
+        # print("image_nom min: ", image_nom.min())
         label_lab = np.array([label_lab])
         # print("image_nom shape: ", image_nom.shape)
-        # label_lab shape:  (1, 256, 256, 3)
-        # image_nom shape:  torch.Size([3, 256, 256])
+        # label_lab shape:  (1, 512, 512, 3)
+        # image_nom shape:  torch.Size([3, 512, 512])
         # align the shape of label_lab to image_nom
         label_lab = label_lab.transpose(3, 0, 1, 2)
-        # label_lab shape:  (3, 1, 256, 256)
+        # label_lab shape:  (3, 1, 512, 512)
         # align the shape of label_lab to image_nom
         label_lab = np.squeeze(label_lab)
         # print("label_lab shape: ", label_lab.shape)
 
-        sample = {'O': image_nom, 'B': label_lab, 'image': np.array(image_lab, dtype='float32').transpose(2, 0, 1) / 255}
+        image_ori =  np.array(image_lab, dtype='float32').transpose(2, 0, 1)
+
+        # log13
+        sample = {'O': image_nom, 'B': label_lab, 'image': np.array(image_lab, dtype='float32').transpose(2, 0, 1) / 255, "image_ori": np.array(image_lab, dtype='float32').transpose(2, 0, 1)}
+        
+        # log15
+        # sample = {'O': image_ori, 'B': label_lab, 'image': np.array(image_lab, dtype='float32').transpose(2, 0, 1) / 255, "image_ori": image_ori}
+
         return sample
 
 
@@ -69,9 +83,9 @@ class TestDataset(Dataset):
     def __init__(self, name):
         super().__init__()
         self.dataset = name
-        # self.root = '/home/zhxing/Datasets/SRD_inpaint4shadow_fix/'
+        self.root = '/home/zhxing/Datasets/SRD_inpaint4shadow_fix/'
         # self.root = '/home/zhxing/Datasets/ISTD+/'
-        self.root = '/home/zhxing/Datasets/DESOBA_xvision/'
+        # self.root = '/home/zhxing/Datasets/DESOBA_xvision/'
 
         self.imgs = open(self.root + 'test_dsc.txt').readlines()
         self.file_num = len(self.imgs)
@@ -94,8 +108,8 @@ class TestDataset(Dataset):
         label_lab = cv2.cvtColor(label, cv2.COLOR_BGR2LAB)
 
         # Resize images
-        image_lab = cv2.resize(image_lab, (256, 256))
-        label_lab = cv2.resize(label_lab, (256, 256))
+        image_lab = cv2.resize(image_lab, (512, 512))
+        label_lab = cv2.resize(label_lab, (512, 512))
 
         # Convert to PIL Image for transformations
         image_lab = Image.fromarray(image_lab)
@@ -105,15 +119,25 @@ class TestDataset(Dataset):
         
         label_lab = np.array([label_lab])
         # print("image_nom shape: ", image_nom.shape)
-        # label_lab shape:  (1, 256, 256, 3)
-        # image_nom shape:  torch.Size([3, 256, 256])
+        # label_lab shape:  (1, 512, 512, 3)
+        # image_nom shape:  torch.Size([3, 512, 512])
         # align the shape of label_lab to image_nom
         label_lab = label_lab.transpose(3, 0, 1, 2)
-        # label_lab shape:  (3, 1, 256, 256)
+        # label_lab shape:  (3, 1, 512, 512)
         # align the shape of label_lab to image_nom
         label_lab = np.squeeze(label_lab)
         # print("label_lab shape: ", label_lab.shape)
 
-        sample = {'O': image_nom, 'B': label_lab, 'image': np.array(image_lab)}
+        # print the range of image_nom
+        # print("image_nom max: ", image_nom.max())
+
+        image_ori =  np.array(image_lab, dtype='float32').transpose(2, 0, 1)
+
+        # sample = {'O': image_nom, 'B': label_lab, 'image': np.array(image_lab)}
+        # log15
+        # sample = {'O': image_ori, 'B': label_lab, 'image': np.array(image_lab), "image_ori": image_ori}
+        # log13
+        sample = {'O': image_nom, 'B': label_lab, 'image': np.array(image_lab), "image_ori": image_ori}
+
 
         return sample
